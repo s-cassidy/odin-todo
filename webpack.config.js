a483bd3853f25369badcd56368f7d2b87cb311c5
Sam Cassidy <sam@cassidys.email>
2023-12-30 22:04:22 +0000

    Initial configuration and template

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './src/index.js'
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist'
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/template.html'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource'
+      }
+    ]
+  }
+}
