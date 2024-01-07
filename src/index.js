1e419396ccdb074f2d9619b075d9d3acdc0de8d1
Sam Cassidy <sam@cassidys.email>
2024-01-07 15:36:30 +0000

    Add reset button for local storage

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import './style.css';
 
-import { initProjectsMenu, updateProjectsMenu } from './projectSelector';
+import { initProjectsMenu, refreshProjectsMenu } from './projectSelector';
 import setupForm from './form';
+import projects from './projectsList';
+import refreshTodoListDisplay from './listDisplay';
+import './resetButton';
 
 setupForm();
 initProjectsMenu();
-updateProjectsMenu();
+refreshProjectsMenu();
+refreshTodoListDisplay(projects.currentProject);
