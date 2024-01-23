c88f61eaa2901a2bda338a44aeacfe55b361c3e9
Sam Cassidy <sam@cassidys.email>
2024-01-23 19:54:43 +0000

    Add confirmation of reset

--- src/resetButton.js
+++ src/resetButton.js
@@ -6,8 +6,10 @@ import refreshTodoListDisplay from './listDisplay';
 const resetButton = document.querySelector('.reset-button');
 
 resetButton.addEventListener('click', () => {
-  new ProjectStorage().clear();
-  projects.reset();
-  refreshProjectsMenu();
-  refreshTodoListDisplay(projects.currentProject);
+  if (confirm('This will reset all projects and todos')) {
+    new ProjectStorage().clear();
+    projects.reset();
+    refreshProjectsMenu();
+    refreshTodoListDisplay(projects.currentProject);
+  }
 });
